62d81ecb33296f6cf7e6e1ac300b4af71df7ee4f
ve.loshkarev <ve.loshkarev.favoritparts@gmail.com>
2019-04-17 11:36:19 +0300

    Перевел пагинацию на встроенные методы Django-rest-framework

--- brandslist/apps/brands/views.py
+++ brandslist/apps/brands/views.py
@@ -1,24 +1,25 @@
 from rest_framework import generics, serializers
+from rest_framework.pagination import PageNumberPagination
 from brandslist.apps.brands.models import Brand
 
-
-# Create your views here.
-
-
-class BrandSerializer(serializers.HyperlinkedModelSerializer):
+class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = ('id', 'name', 'description')
 
+class BrandsPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'skip'
+
 
 class BrnadsList(generics.ListAPIView):
     serializer_class = BrandSerializer
+    pagination_class = BrandsPagination
+    http_method_names = ['get']
 
     def get_queryset(self):
-        skip = self.request.query_params.get('skip', '0')
-        skip = int(skip) if skip != '' else 0
-        queryset = Brand.objects.all()[skip:skip + 20]
+        queryset = Brand.objects.all()
         name = self.request.query_params.get('name', '')
         if name != '':
-            queryset = Brand.objects.filter(name__icontains=name)[skip:skip + 20]
+            queryset = Brand.objects.filter(name__icontains=name)
         return queryset
